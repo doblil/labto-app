166052decac2c6378d6dbad1200097af8c6c62b9
doblil <doblil@mail.ru>
2023-03-22 23:30:24 +0300

    column table for print

--- src/blocs/screens/prep/table/column/columnHistory.js
+++ src/blocs/screens/prep/table/column/columnHistory.js
@@ -9,15 +9,16 @@ export const ColumnHistory = (props) => {
       .map(item => {
         const {fromDate, toDate, restSolvent, comment, userName, destination, countInj, test, mobilePhase, _id} = item
         return(
-            <div className="column__history" key={_id}>
-                <div className="column__value" style={{width:'20%'}}> {stringifyDate(fromDate)} -- {stringifyDate(toDate)}<br/><br />{userName}</div>
-                <div className="column__value" style={{width:'10%'}}>{destination?.code}<br/><br />{destination?.name}</div>
-                <div className="column__value" style={{width:'20%'}}>{test}, <br/><br />{countInj} инжекций</div>
-                <div className="column__value" style={{width:'15%'}}> {mobilePhase}</div>
-                <div className="column__value" style={{width:'10%'}}> {restSolvent}</div>
-                <div className="column__value" style={{width:'25%'}}> {comment}</div>
-       
-            </div>
+            <>          
+                <div className="column__history" key={_id}>
+                    <div className="column__value" style={{width:'20%'}}> {stringifyDate(fromDate)} -- {stringifyDate(toDate)}<br/><br />{userName}</div>
+                    <div className="column__value" style={{width:'10%'}}>{destination?.code}<br/><br />{destination?.name}</div>
+                    <div className="column__value" style={{width:'20%'}}>{test}, <br/><br />{countInj} инжекций</div>
+                    <div className="column__value" style={{width:'15%'}}> {mobilePhase}</div>
+                    <div className="column__value" style={{width:'10%'}}> {restSolvent}</div>
+                    <div className="column__value" style={{width:'25%'}}> {comment}</div>
+                </div>
+            </>
         )
     }).reverse()
     if(!inUse.length){
@@ -30,12 +31,19 @@ export const ColumnHistory = (props) => {
         <>
             <div className="overlay">
                 <div className="overlay__window" style={{width: '90vw', maxWidth: 'none'}}>
-                    <div className="close" onClick={() => {setShowHistory(false)}}></div>
+                    <div className="close close_big" style={{top: '5px', right: '5px'}} onClick={() => {setShowHistory(false)}}></div>
+                    <div className="print" style={{top: '5px', right: '35px'}}></div>
                     <div className="overlay__heading"> 
-                        <p>
-                            История использования колонки ID: <b>{itemId}</b>,   <b>{name}</b>,   серийный номер: <b>{sn}</b>,   каталожный номер: <b>{cat}</b> 
-                        </p>
+                        История использования колонки ID: <b>{itemId}</b>,   <b>{name}</b>,   серийный номер: <b>{sn}</b>,   каталожный номер: <b>{cat}</b> 
                     </div>
+                    <div className="column__history-head">
+                        <div className="column__value-head" style={{width:'20%'}}>дата, имя</div>
+                        <div className="column__value-head" style={{width:'10%'}}>проект</div>
+                        <div className="column__value-head" style={{width:'20%'}}>количество инжекций</div>
+                        <div className="column__value-head" style={{width:'15%'}}>подвижная фаза</div>
+                        <div className="column__value-head" style={{width:'10%'}}>растворитель хранения</div>
+                        <div className="column__value-head" style={{width:'25%'}}>комментарий</div>
+                    </div>  
                     <div className="overflow" style={{height: '70vh', width:'100%',}}>
                         {content}
                     </div> 
